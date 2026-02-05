e85938ba27d5609787a0083d48b08182dcf273cb
ccerviedelara-lang <cc.ervie.delara@cvsu.edu.ph>
2026-02-05 11:29:18 +0800

    update

--- frontend/src/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("loginForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Login successful (demo only)");
-  window.location.href = "dashboard.html";
-});
